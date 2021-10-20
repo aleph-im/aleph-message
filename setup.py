f4421fe24ecc91c77a0b651ca95ec1d6cd997c62
Hugo Herter <git@hugoherter.com>
2021-10-20 11:17:49 +0200

    Release 0.1.15

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.md') as file:
     long_description = file.read()
 
 setup(name='aleph-message',
-      version='0.1.14',
+      version='0.1.15',
       description='Aleph.im message specification ',
       long_description=long_description,
       long_description_content_type='text/markdown',
