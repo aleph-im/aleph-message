e0deb5cc6b37a8614bb2c9b315816f191a9b417c
Olivier Desenfans <desenfans.olivier@gmail.com>
2022-09-15 12:39:03 +0200

    Feature: ItemHash type
    Problem: validation of item hashes was too lax, resulting in
    some messages containing nonsensical hashes.
    
    Solution: improve the validation of item hashes in messages.
    This commit introduces a new ItemHash Pydantic validation type
    that performs some basic checks on the value of the hash.

--- aleph_message/tests/test_types.py
+++ aleph_message/tests/test_types.py
@@ -1,4 +1,7 @@
-from aleph_message.models import ItemType
+import pytest
+from pydantic import BaseModel, ValidationError
+
+from aleph_message.models import ItemHash, ItemType
 
 STORAGE_HASH = "b236db23bf5ad005ad7f5d82eed08a68a925020f0755b2a59c03f784499198eb"
 IPFS_HASH = "QmPxCe3eHVCdTG5uKnSZTsPGrYvMFTWAAt4PSfK7ETkz4d"
@@ -9,3 +12,23 @@ def test_item_type():
     assert ItemType.is_storage(STORAGE_HASH)
     assert ItemType.from_hash(IPFS_HASH) == ItemType.ipfs
     assert ItemType.is_ipfs(IPFS_HASH)
+
+
+class TestHashModel(BaseModel):
+    hash: ItemHash
+
+
+def test_item_hash():
+    storage_object_dict = {"hash": STORAGE_HASH}
+    storage_object = TestHashModel.parse_obj(storage_object_dict)
+    assert storage_object.hash == STORAGE_HASH
+    assert storage_object.hash.item_type == ItemType.storage
+
+    ipfs_object_dict = {"hash": IPFS_HASH}
+    ipfs_object = TestHashModel.parse_obj(ipfs_object_dict)
+    assert ipfs_object.hash == IPFS_HASH
+    assert ipfs_object.hash.item_type == ItemType.ipfs
+
+    invalid_object_dict = {"hash": "fake-hash"}
+    with pytest.raises(ValidationError):
+        _ = TestHashModel.parse_obj(invalid_object_dict)
