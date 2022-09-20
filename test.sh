48913293cb824c68d789e8c7b057af9ef59d8536
Hugo Herter <git@hugoherter.com>
2022-09-20 15:29:03 +0100

    CI: Types were unchecked
    Solution: Add and run mypy

--- test.sh
+++ test.sh
@@ -4,3 +4,4 @@ set -euf
 
 podman build --loglevel 3 -t aleph-message .
 podman run --rm -ti -v $(pwd)/aleph_message:/opt/aleph_message aleph-message pytest -vv
+podman run --rm -ti -v $(pwd)/aleph_message:/opt/aleph_message aleph-message mypy --ignore-missing-imports aleph_message
