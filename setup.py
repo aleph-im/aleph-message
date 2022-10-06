26247a7ff38b797d476ccdebe3b12010ace5670f
Hugo Herter <git@hugoherter.com>
2022-10-06 19:50:37 +0200

    Release 0.2.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.md') as file:
     long_description = file.read()
 
 setup(name='aleph-message',
-      version='0.2.2',
+      version='0.2.3',
       description='Aleph.im message specification ',
       long_description=long_description,
       long_description_content_type='text/markdown',
