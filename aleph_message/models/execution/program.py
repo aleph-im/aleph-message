d798a0db3428fc4cbc4437d336b0fb6fed139b75
mhh <mike.hukiewitz@robotter.ai>
2023-11-22 13:45:23 +0100

    Add Entrypoint class and update CodeContent model to reference Entrypoint.
    Allows distinction between different types of plain code executions and more freedom in defining how it should be run.

--- aleph_message/models/execution/program.py
+++ aleph_message/models/execution/program.py
@@ -17,11 +17,25 @@ class FunctionRuntime(HashableModel):
     comment: str
 
 
+class Entrypoint(HashableModel):
+    """
+    Entrypoint to a program.
+
+    The type of the entrypoint determines how the program is executed.
+    The name of the entrypoint determines which file or function is executed.
+    Additional arguments can be provided.
+    """
+
+    name: str
+    type: Literal["python-asgi"] | Literal["bash"] | Literal["binary"] | Literal["node.js"]
+    args: list[str]
+
+
 class CodeContent(HashableModel):
     """Reference to the StoreMessage that contains the code or program to be executed."""
 
     encoding: Encoding
-    entrypoint: str
+    entrypoint: Entrypoint
     ref: ItemHash  # Must reference a StoreMessage
     use_latest: bool = False
 
