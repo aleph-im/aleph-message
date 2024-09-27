2404dfe57762fed3238d73cab2d906c667d95bad
Antonyjin <antony.jin@epitech.eu>
2024-09-27 22:57:39 +0900

    Fix: Refactor classes and functions to align with Pydantic v2.
    - Updated code to explicitly specify optional keys where necessary.
    - Replaced direct `.get` calls with `data.get()` to handle new validation logic.
    - Migrated model configuration to use `model_config = ConfigDict(extra="forbid")`
    or `model_config = ConfigDict(extra="allow")` in place of Pydantic v1's configuration style.

--- aleph_message/models/execution/volume.py
+++ aleph_message/models/execution/volume.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 from enum import Enum
 from typing import Literal, Optional, Union
 
-from pydantic import ConstrainedInt, Extra
+from pydantic import Field, ConfigDict, BaseModel, field_validator
 
 from ...utils import Gigabytes, gigabyte_to_mebibyte
 from ..abstract import HashableModel
@@ -18,27 +18,35 @@ class AbstractVolume(HashableModel, ABC):
     @abstractmethod
     def is_read_only(self): ...
 
-    class Config:
-        extra = Extra.forbid
+    model_config = ConfigDict(extra="forbid")
 
 
 class ImmutableVolume(AbstractVolume):
-    ref: ItemHash
+    ref: ItemHash = None
     use_latest: bool = True
 
     def is_read_only(self):
         return True
 
 
-class EphemeralVolumeSize(ConstrainedInt):
-    gt = 0
-    le = 1000  # Limit to 1 GiB
-    strict = True
+class EphemeralVolumeSize(BaseModel):
+    ephemeral_volume_size: int = Field(gt=0,
+                                       le=1000, #Limit to 1GiB
+                                       strict=True)
+
+    def __hash__(self):
+        return hash(self.ephemeral_volume_size)
 
 
 class EphemeralVolume(AbstractVolume):
     ephemeral: Literal[True] = True
-    size_mib: EphemeralVolumeSize
+    size_mib: EphemeralVolumeSize = 0
+
+    @field_validator('size_mib', mode="before")
+    def convert_size_mib(cls, v):
+        if isinstance(v, int):
+            return EphemeralVolumeSize(ephemeral_volume_size=v)
+        return v
 
     def is_read_only(self):
         return False
@@ -65,10 +73,16 @@ class PersistentVolumeSizeMib(ConstrainedInt):
 
 
 class PersistentVolume(AbstractVolume):
-    parent: Optional[ParentVolume]
-    persistence: VolumePersistence
-    name: str
-    size_mib: PersistentVolumeSizeMib
+    parent: Optional[ParentVolume] = None
+    persistence: VolumePersistence = None
+    name: Optional[str] = None
+    size_mib: PersistentVolumeSizeMib = 0
+
+    @field_validator('size_mib', mode="before")
+    def convert_size_mib(cls, v):
+        if isinstance(v, int):
+            return PersistentVolumeSizeMib(persistent_volume_size=v)
+        return v
 
     def is_read_only(self):
         return False
