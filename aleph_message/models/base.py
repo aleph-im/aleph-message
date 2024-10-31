1224cfa1d551993e89e92aa2255151af0cbb6a8d
Andres D. Molins <amolinsdiaz@yahoo.es>
2024-10-31 21:00:29 +0100

    Feature: Added new EVM compatible chains.

--- aleph_message/models/base.py
+++ aleph_message/models/base.py
@@ -4,17 +4,31 @@ from enum import Enum
 class Chain(str, Enum):
     """Supported chains"""
 
+    ARBITRUM = "ARB"
     AVAX = "AVAX"
     BASE = "BASE"
+    BLAST = "BLAST"
+    BOB = "BOB"
     BSC = "BSC"
     CSDK = "CSDK"
+    CYBER = "CYBER"
     DOT = "DOT"
     ETH = "ETH"
+    FRAXTAL = "FRAX"
+    INK = "INK"
+    LINEA = "LINEA"
+    LISK = "LISK"
+    METIS = "METIS"
+    MODE = "MODE"
     NEO = "NEO"
     NULS = "NULS"
     NULS2 = "NULS2"
+    OPTIMISM = "OP"
+    POL = "POL"
     SOL = "SOL"
     TEZOS = "TEZOS"
+    WORLDCHAIN = "WLD"
+    ZORA = "ZORA"
 
 
 class HashType(str, Enum):
