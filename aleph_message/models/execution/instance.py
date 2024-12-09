41b4c3a10745c958c40c2123e4f09d91cee53e4e
Bram <cortex@worlddomination.be>
2024-12-09 17:36:03 +0100

    feat: allows CRN to publish a terms_and_condition (#121)
    * feat: allows CRN to publish a terms_and_condition
    
    * Fix: Resolved conflict unifying criteria on requirements validator.
    
    ---------
    
    Co-authored-by: Andres D. Molins <amolinsdiaz@yahoo.es>

--- aleph_message/models/execution/instance.py
+++ aleph_message/models/execution/instance.py
@@ -34,17 +34,35 @@ class InstanceContent(BaseExecutableContent):
     )
 
     @root_validator()
-    def check_gpu_requirement(cls, values):
-        if values.get("requirements") and values.get("requirements").gpu:
-            if (
-                not values.get("requirements").node
-                or not values.get("requirements").node.node_hash
-            ):
-                raise ValueError("Node hash assignment is needed for GPU support")
+    def check_requirements(cls, values):
+        if values.get("requirements"):
+            # GPU filter only supported for QEmu instances with node_hash assigned
+            if values.get("requirements").gpu:
+                if (
+                    not values.get("requirements").node
+                    or not values.get("requirements").node.node_hash
+                ):
+                    raise ValueError("Node hash assignment is needed for GPU support")
 
+                if (
+                    values.get("environment")
+                    and values.get("environment").hypervisor != HypervisorType.qemu
+                ):
+                    raise ValueError("GPU option is only supported for QEmu hypervisor")
+
+            # Terms and conditions filter only supported for stream payments with node_hash assigned
             if (
-                values.get("environment")
-                and values.get("environment").hypervisor != HypervisorType.qemu
+                values.get("requirements").node
+                and values.get("requirements").node.terms_and_conditions
             ):
-                raise ValueError("GPU option is only supported for QEmu hypervisor")
+                if not values.get("requirements").node.node_hash:
+                    raise ValueError(
+                        "Terms_and_conditions field needs a requirements.node.node_hash value"
+                    )
+
+                if not values.get("payment").is_stream:
+                    raise ValueError(
+                        "Only PAYG/stream instances can have a terms_and_conditions"
+                    )
+
         return values
