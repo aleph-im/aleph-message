4ef17a0050a09b2d11cb9197fa1be40c087112d6
Andres D. Molins <amolinsdiaz@yahoo.es>
2025-10-08 13:11:01 +0200

    Fix: Fixed class method signature to use the good one from Pydantic, they introduced in their last version 2.12.

--- aleph_message/models/execution/instance.py
+++ aleph_message/models/execution/instance.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import List, Optional
 
 from pydantic import Field, model_validator
+from typing_extensions import Self
 
 from aleph_message.models.abstract import HashableModel
 
@@ -43,46 +44,37 @@ class InstanceContent(BaseExecutableContent):
     )
 
     @model_validator(mode="after")
-    def check_requirements(cls, values):
-        if getattr(values, "requirements", None):
+    def check_requirements(self) -> Self:
+        if self.requirements:
             if (
-                getattr(values.payment, "is_stream", None)
-                or getattr(values.payment, "is_credit", None)
-            ) and (
-                not getattr(values.requirements, "node", None)
-                or not getattr(values.requirements.node, "node_hash", None)
-            ):
+                self.payment and (self.payment.is_stream or self.payment.is_credit)
+            ) and (not self.requirements.node or not self.requirements.node.node_hash):
                 raise ValueError(
                     "Node hash assignment is needed for PAYG or Credit payments"
                 )
             # GPU filter only supported for QEmu instances with node_hash assigned
-            if getattr(values.requirements, "gpu", None):
-                if not getattr(values.requirements, "node", None) or not getattr(
-                    values.requirements.node, "node_hash", None
-                ):
+            if self.requirements.gpu:
+                if not self.requirements.node or not self.requirements.node.node_hash:
                     raise ValueError("Node hash assignment is needed for GPU support")
 
                 if (
-                    getattr(values, "environment", None)
-                    and getattr(values.environment, "hypervisor", None)
-                    != HypervisorType.qemu
+                    self.environment
+                    and self.environment.hypervisor != HypervisorType.qemu
                 ):
                     raise ValueError("GPU option is only supported for QEmu hypervisor")
 
             # Terms and conditions filter only supported for PAYG/coco instances with node_hash assigned
-            if getattr(values.requirements, "node", None) and getattr(
-                values.requirements.node, "terms_and_conditions", None
-            ):
-                if not getattr(values.requirements.node, "node_hash", None):
+            if self.requirements.node and self.requirements.node.terms_and_conditions:
+                if not self.requirements.node.node_hash:
                     raise ValueError(
                         "Terms_and_conditions field needs a requirements.node.node_hash value"
                     )
 
-                if not getattr(values.payment, "is_stream", None) and not getattr(
-                    values.environment, "trusted_execution", None
-                ):
+                if (
+                    not self.payment or not self.payment.is_stream
+                ) and not self.environment.trusted_execution:
                     raise ValueError(
                         "Only PAYG/coco instances can have a terms_and_conditions"
                     )
 
-        return values
+        return self
